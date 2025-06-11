675143dc4bea0e3dfa3bf1f6dc93bfaab421ff0d
shouryaco <chowdhury.shourya19@gmail.com>
2025-06-12 00:32:59 +0530

    Revert back to *

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # Allow CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://www.lifestepx.com"],  # Use specific domain in production
+    allow_origins=["*"],  # Use specific domain in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
